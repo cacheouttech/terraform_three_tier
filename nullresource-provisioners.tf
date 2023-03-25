55ece5237a5395ee43d69ec4add5e5fe05a36499
cacheouttech <108108554+cacheouttech@users.noreply.github.com>
2023-03-25 16:59:57 -0400

    Update nullresource-provisioners.tf

--- nullresource-provisioners.tf
+++ nullresource-provisioners.tf
@@ -21,4 +21,22 @@ resource "null_resource" "name" {
       "sudo chmod 400 /tmp/two-tier-key.pem"
     ]
   }
+## Local Exec Provisioner:  local-exec provisioner (Creation-Time Provisioner - Triggered during Create Resource)
+  provisioner "local-exec" {
+    command = "echo VPC created on `date` and VPC ID: ${module.vpc.vpc_id} >> creation-time-vpc-id.txt"
+    working_dir = "local-exec-output-files/"
+    #on_failure = continue
+  }
+## Local Exec Provisioner:  local-exec provisioner (Destroy-Time Provisioner - Triggered during deletion of Resource)
+/*  provisioner "local-exec" {
+    command = "echo Destroy time prov `date` >> destroy-time-prov.txt"
+    working_dir = "local-exec-output-files/"
+    when = destroy
+    #on_failure = continue
+  }  
+  */
+
 }
+
+# Creation Time Provisioners - By default they are created during resource creations (terraform apply)
+# Destory Time Provisioners - Will be executed during "terraform destroy" command (when = destroy)
