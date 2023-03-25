eeec90554b9b25b20dfd7dbc4bbf39bdb9cee2d6
cacheouttech <108108554+cacheouttech@users.noreply.github.com>
2023-03-25 17:00:24 -0400

    Update eip.tf

--- eip.tf
+++ eip.tf
@@ -4,5 +4,14 @@ resource "aws_eip" "bastion_eip" {
   depends_on = [ module.ec2_public, module.vpc ]
   instance = module.ec2_public.id[0]
   vpc      = true
+  
+## Local Exec Provisioner:  local-exec provisioner (Destroy-Time Provisioner - Triggered during deletion of Resource)
+  provisioner "local-exec" {
+    command = "echo Destroy time prov `date` >> destroy-time-prov.txt"
+    working_dir = "local-exec-output-files/"
+    when = destroy
+    #on_failure = continue
+  }  
 }
+
 
